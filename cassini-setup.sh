da5ea8707b9d0acbe6b35aeb31a1a0071bfd93d7
Piper McCorkle <piper@ladderlife.com>
2019-10-23 15:08:03 -0700

    Add prompt before using SSH key

--- cassini-setup.sh
+++ cassini-setup.sh
@@ -8,8 +8,9 @@ git remote add origin https://github.com/piperswe/cassini.git
 git pull origin master
 mkdir .ssh
 ssh-keygen -t ed25519 -N "" -f .ssh/id_ed25519 -C "cassini.piperswe.me"
-echo "Add the following SSH key to GitHub:"
+echo "Add the following SSH key to GitHub before proceeding:"
 cat .ssh/id_ed25519.pub
+read
 git remote remove origin
 git remote add origin git@github.com:piperswe/cassini.git
 git submodule init
