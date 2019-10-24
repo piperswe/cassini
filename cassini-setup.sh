185f56f6e7c1bf85141fc7b2a6ea725f44268f4a
Piper McCorkle <piper@ladderlife.com>
2019-10-24 11:33:31 -0700

    Add emacs config

--- cassini-setup.sh
+++ cassini-setup.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 export HOME="${DIR:=${HOME}}"
+echo "Cloning Git repo..."
 mkdir -p "${DIR}"
 cd "${DIR}"
 git init
 git remote add origin https://github.com/piperswe/cassini.git
-git pull origin master
+git pull --set-upstream origin master
+echo "  Generating SSH key..."
 mkdir .ssh
 ssh-keygen -t ed25519 -N "" -f .ssh/id_ed25519 -C "cassini.piperswe.me"
-echo "Add the following SSH key to GitHub before proceeding:"
+echo "    Add the following SSH key to GitHub before proceeding:"
 cat .ssh/id_ed25519.pub
 read
+echo "  Adding SSH remote..."
 git remote remove origin
 git remote add origin git@github.com:piperswe/cassini.git
+git pull
+echo "  Downloading submodules..."
 git submodule init
 git submodule update
-echo "Enter your password to change your shell:"
+echo "Configuring zsh..."
+echo "  Downloading Antigen dependencies..."
+zsh .zshrc
+echo "  Changing shell..."
+echo "    Enter your password to change your shell:"
 chsh -s /bin/zsh
