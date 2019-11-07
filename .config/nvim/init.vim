2446bfef87452ee8c304a5a58bb8541d939ccb76
Piper McCorkle <piper@ladderlife.com>
2019-11-07 15:09:26 -0800

    Gruvbox

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -7,5 +7,16 @@ endif
 call plug#begin(stdpath('data') . '/plugged')
 
 Plug 'vim-scripts/paredit.vim'
+Plug 'morhetz/gruvbox'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
 
 call plug#end()
+
+set termguicolors
+let $NVIM_TUI_ENABLE_TRUE_COLOR=1
+let g:gruvbox_italic=1
+colorscheme gruvbox
+
+let g:airline_powerline_fonts = 1
+let g:airline#extensions#tabline#enabled = 1
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,11 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (cider))))
+ '(custom-enabled-themes (quote (gruvbox-dark-medium)))
+ '(custom-safe-themes
+   (quote
+    ("8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default)))
+ '(package-selected-packages (quote (gruvbox-theme cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
